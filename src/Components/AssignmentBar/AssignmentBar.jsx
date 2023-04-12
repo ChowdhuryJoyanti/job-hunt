bea5f0df9c32740b00871053f5331770b4f1f097
Joyantichy <120155482+Joyantichy@users.noreply.github.com>
2023-04-12 17:47:05 +0600

    assignment section added

--- src/Components/AssignmentBar/AssignmentBar.jsx
+++ src/Components/AssignmentBar/AssignmentBar.jsx
@@ -1,16 +1,40 @@
 import React, { useEffect, useState } from 'react';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+import axios from 'axios';
 
 const AssignmentBar = () => {
     const [assignments,setAssignments] =useState([]);
 
     useEffect(() =>{
-        fetch('assignment.json')
-        .then(res => res.json())
-        .then(data =>console.log(data))
+        // fetch('assignment.json')
+        // .then(res => res.json())
+        // .then(data =>console.log(data))
+        axios.get('assignment.json')
+        .then(data => {
+            const loadedData = data.data;
+            console.log(loadedData);
+            const assignmentData = loadedData.map(assignment =>{
+                const parts = assignment.slug.split('_')
+                const numbers = parts[1];
+                const assignmentInfo ={
+                    name:assignment.name,
+                    number:assignment.number
+                }
+                return assignmentInfo;
+            })
+            console.log(assignmentData);
+            setAssignments(assignmentData)
+        });
 
     },[])
     return (
-        <div>
+        <div className='container'>
+            <BarChart width={1000} height={500} data={assignments}>
+            <Bar dataKey="number" fill="#8884d8" />
+            <XAxis dataKey="name"></XAxis>
+           <YAxis/>
+            </BarChart>
             
         </div>
     );
--- src/Components/Statistics/Statistics.jsx
+++ src/Components/Statistics/Statistics.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import AssignmentBar from '../AssignmentBar/AssignmentBar';
 
 const Statistics = () => {
     return (
         <div>
             {/* <h3>statiscis</h3> */}
             <Banner></Banner>
+            <AssignmentBar></AssignmentBar>
 
         </div>
     );
